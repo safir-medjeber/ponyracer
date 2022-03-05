a8f1e4bc56dd104089bf8b2fb8b20a0c89ff06fb
Safir MEDJEBER <safir.medjeber@gmail.com>
2022-03-05 17:36:02 +0100

    9. Pony component

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -27,6 +27,12 @@ describe('Ponyracer', () => {
   it('should display a race list', () => {
     cy.visit('/');
     cy.get('h2').should('have.length', 2);
-    cy.get('.container ul li').should('have.length', 10);
+  });
+
+  it('should display ponies', () => {
+    cy.visit('/');
+    cy.get('figure').should('have.length', 10);
+    cy.get('img').should('have.length', 10);
+    cy.get('figcaption').should('have.length', 10);
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RacesComponent } from './races/races.component';
 import { RaceComponent } from './race/race.component';
+import { PonyComponent } from './pony/pony.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent]
+      declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent, PonyComponent]
     })
   );
 
